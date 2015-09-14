c3eec26120494bdedcfca65f3798133c466189c6
Jakub Veverka <veverka.kuba@gmail.com>
2015-09-14 12:33:18 +0200

    Use ec2metadata to fetch region info
    - update aws libraries

--- src/github.com/UKHomeOffice/s3secrets/main.go
+++ src/github.com/UKHomeOffice/s3secrets/main.go
@@ -4,9 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path"
 	"strings"
@@ -46,18 +46,15 @@ func main() {
 		os.Exit(1)
 	}
 	if region == "" {
-		resp, err := http.Get("http://169.254.169.254/latest/meta-data/placement/availability-zone")
+		client := ec2metadata.New(nil)
+		zone, err := client.GetMetadata("/placement/availability-zone")
 		if err == nil {
-			defer resp.Body.Close()
-			contents, err := ioutil.ReadAll(resp.Body)
-			if err == nil {
-				region = string(contents[0 : len(contents)-1])
-			}
+			region = string(region[0 : len(zone)-1])
 		}
 	}
 	var cfg *aws.Config
 	if region != "" {
-		cfg = &aws.Config{Region: region}
+		cfg = &aws.Config{Region: &region}
 	}
 
 	sp := splitPath(bucket)
